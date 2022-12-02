6b8203474de728535e7a544d74d4b14d63239856
abc273 <67716675+abc273@users.noreply.github.com>
2022-12-02 17:02:48 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="index">
+    <van-icon name="chat" color="red" />
+    <van-button type="primary">主要按钮</van-button>
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import './index.less';
+
+const msg = ref('Hello world');
+</script>
